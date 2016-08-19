8dc01442cac18455738faed0a2f41e7b0e7960ad
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-19 17:32:26 -0500

    Major Updates to JS/HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,80 +9,154 @@ var azmodan = {
 	"name": "Azmodan",
 	"healthPoints": 150,
 	"attackPower": 15,
-	"counterAttackPower": 20
+	"counterAttackPower": 10
 };
 
 var johanna = {
 	"name": "Johanna",
-	"healthPoints": 200,
-	"attackPower": 20,
-	"counterAttackPower": 30
+	"healthPoints": 400,
+	"attackPower": 15,
+	"counterAttackPower": 5
 };
 
 var murky = {
 	"name": "Murky",
-	"healthPoints": 120,
-	"attackPower": 10,
-	"counterAttackPower": 15
+	"healthPoints": 50,
+	"attackPower": 25,
+	"counterAttackPower": 5
 };
 
+var jainaStatsUpdate = $(".jaina");
+	jainaStatsUpdate.attr("healthPoints", jaina.healthPoints);
+	jainaStatsUpdate.attr("attackPower", jaina.attackPower);
+	jainaStatsUpdate.attr("counterAttackPower", jaina.counterAttackPower);
 
-// $(".yourCharacter").one("click", function(){
-// 	console.log(this + "has been clicked");
-// 	$(this).removeClass(".chosenCharacters").addClass(".champCharacter");
-// 	$(".yourCharacter").appendTo(".enemiesCharacters").removeClass(".yourCharacter").addClass("enemyCharacter1");
+var azmodanStatsUpdate = $(".azmodan");
+	azmodanStatsUpdate.attr("healthPoints", azmodan.healthPoints);
+	azmodanStatsUpdate.attr("attackPower", azmodan.attackPower);
+	azmodanStatsUpdate.attr("counterAttackPower", azmodan.counterAttackPower);
+
+var johannaStatsUpdate = $(".johanna");
+	johannaStatsUpdate.attr("healthPoints", johanna.healthPoints);
+	johannaStatsUpdate.attr("attackPower", johanna.attackPower);
+	johannaStatsUpdate.attr("counterAttackPower", johanna.counterAttackPower);
+
+var murkyStatsUpdate = $(".murky");
+	murkyStatsUpdate.attr("healthPoints", murky.healthPoints);
+	murkyStatsUpdate.attr("attackPower", murky.attackPower);
+	murkyStatsUpdate.attr("counterAttackPower", murky.counterAttackPower);
 
 
+$(".chosenCharacters").click(function(){
+	$(this).removeClass("nonChosenCharacters");
+	$(".nonChosenCharacters").removeClass("chosenCharacters")
+		.removeClass("nonChosenCharacters")
+		.appendTo(".enemiesCharacters")
+		.addClass("enemyCharacter");
 
-$(".yourCharacter1").one("click", function(){
-	$(".yourCharacter2").appendTo(".enemyCharacter2").addClass("enemyCharacter2").removeClass("yourCharacter2");
-	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
 
-	$(".yourCharacter3").appendTo(".enemyCharacter3").addClass("enemyCharacter3").removeClass("yourCharacter3");
-	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
+	$(".enemyCharacter .contentContainer").removeClass("contentContainer")
+		.addClass("contentContainer2");
 
-	$(".yourCharacter4").appendTo(".enemyCharacter4").addClass("enemyCharacter4").removeClass("yourCharacter4");
-	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-});
-
-$(".yourCharacter2").one("click", function(){
-	$(".yourCharacter1").appendTo(".enemyCharacter1").addClass("enemyCharacter1").removeClass("yourCharacter1");
-	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-	$(".yourCharacter3").appendTo(".enemyCharacter3").addClass("enemyCharacter3").removeClass("yourCharacter3");
-	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
-
-	$(".yourCharacter4").appendTo(".enemyCharacter4").addClass("enemyCharacter4").removeClass("yourCharacter4");
-	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-});
-
-$(".yourCharacter3").one("click", function(){
-	$(".yourCharacter1").appendTo(".enemyCharacter1").addClass("enemyCharacter1").removeClass("yourCharacter1");
-	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-	$(".yourCharacter2").appendTo(".enemyCharacter2").addClass("enemyCharacter2").removeClass("yourCharacter2");
-	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
-
-	$(".yourCharacter4").appendTo(".enemyCharacter4").addClass("enemyCharacter4").removeClass("yourCharacter4");
-	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-});
-
-$(".yourCharacter4").one("click", function(){
-	$(".yourCharacter1").appendTo(".enemyCharacter1").addClass("enemyCharacter1").removeClass("yourCharacter1");
-	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-	$(".yourCharacter2").appendTo(".enemyCharacter2").addClass("enemyCharacter2").removeClass("yourCharacter2");
-	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
-
-	$(".yourCharacter3").appendTo(".enemyCharacter3").addClass("enemyCharacter3").removeClass("yourCharacter3");
-	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
-
+    if ($(".defenderSection").is(":empty") && $(this).hasClass("enemyCharacter")) {
+    	$(this).appendTo(".defenderSection")
+    		.removeClass("enemyCharacter")
+    		.addClass("defenderCharacter");
+    		
+		$(".defenderCharacter .contentContainer2").removeClass("contentContainer2")
+			.addClass("contentContainer3");
+    }
 });
 
 
+$(".attackButton").on("click", function(){
+	var defenderCurrentHealth = parseInt($(".defenderCharacter").attr("healthPoints")) - parseInt($(".chosenCharacters").attr("attackPower"));
+	$(".defenderCharacter").attr("healthPoints", defenderCurrentHealth);
+	defenderCurrentHealth = defenderCurrentHealth;
+		console.log(defenderCurrentHealth);
+
+	$(".defenderCharacter .healthPoints").html(defenderCurrentHealth);	
+	console.log(defenderCurrentHealth);
+	// console.log($(".defenderSection"));
+	// console.log(jaina.healthPoints -= parseInt(johanna.counterAttackPower)); 
+})
+
+
+
+
+// $(".btn").click(function(){
+ var defenderHealthLoss = parseInt($(".defender").attr('healthPoints')) - parseInt($(".champ").attr('attackPower'));
+// $(".defender").attr("healthPoints", defenderHealthLoss);
+// var updateNumber = (defenderHealthLoss);
+// document.querySelector(".grumpyP").innerHTML = updateNumber;
+//  console.log("Grumpy cat's health: " + defenderHealthLoss);
+ 		//var counterAttack = parseInt($(".champ").attr('healthPoints')) - parseInt($(".defender").attr('counterAttackPower'));
+ 		//defender counter attacking champ
+// })
+
+
+
+
+
+
+// $(".enemiesCharacters").click(function(){
+// 	// alert("Enemy Character has been clicked");
+// 	console.log(this);
+// 	if($(".defenderSection").is(":empty")){
+// 		alert("Empty");
+// 	}else{
+// 		alert("Not working");
+// 	}
+// });
+
+
+// $(".yourCharacter1").one("click", function(){
+// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
+// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
+
+// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
+// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
+
+// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
+// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
+
+// });
+
+// $(".yourCharacter2").one("click", function(){
+// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
+// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
+
+// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
+// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
+
+// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
+// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
+
+// });
+
+// $(".yourCharacter3").one("click", function(){
+// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
+// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
+
+// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
+// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
+
+// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
+// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
+
+// });
+
+// $(".yourCharacter4").one("click", function(){
+// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
+// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
+
+// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
+// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
+
+// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
+// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
+
+// });
 
 
 
@@ -91,172 +165,37 @@ $(".yourCharacter4").one("click", function(){
 
 
 
-$(".enemyCharacter1").one("click", function(){
-	$(".enemyCharacter1").appendTo(".defenderCharacter1");
-	// $(".enemyCharacter1").addClass(".defenderCharacter1");
-	$(".enemyCharacter1").removeClass("enemyCharacter1");
-	$(".jainaContentContainer2").addClass("jainaContentContainer3");
-	$(".jainaContentContainer2").removeClass("jainaContentContainer2");	
-});
-
-$(".enemyCharacter2").one("click", function(){
-	$(".enemyCharacter2").appendTo(".defenderCharacter1");
-	// $(".enemyCharacter2").addClass(".defenderCharacter1");
-	$(".enemyCharacter2").removeClass("enemyCharacter2");
-	$(".azmodanContentContainer2").addClass("azmodanContentContainer3");
-	$(".azmodanContentContainer2").removeClass("azmodanContentContainer2");
-});
-
-$(".enemyCharacter3").one("click", function(){
-	$(".enemyCharacter3").appendTo(".defenderCharacter1");
-	// $(".enemyCharacter3").addClass(".defenderCharacter1");
-	$(".enemyCharacter3").removeClass("enemyCharacter3");
-	$(".johannaContentContainer2").addClass("johannaContentContainer3");
-	$(".johannaContentContainer2").removeClass("johannaContentContainer2");
-});
-
-$(".enemyCharacter4").one("click", function(){
-	$(".enemyCharacter4").appendTo(".defenderCharacter1");
-	// $(".enemyCharacter4").addClass(".defenderCharacter1");
-	$(".enemyCharacter4").removeClass("enemyCharacter4");
-	$(".murkyContentContainer2").addClass("murkyContentContainer3");
-	$(".murkyContentContainer2").removeClass("murkyContentContainer2");
-});
 
 
+// $(".enemyCharacter1").one("click", function(){
+// 	$(".enemyCharacter1").appendTo(".defenderCharacter1");
+// 	// $(".enemyCharacter1").addClass(".defenderCharacter1");
+// 	$(".enemyCharacter1").removeClass("enemyCharacter1");
+// 	$(".jainaContentContainer2").addClass("jainaContentContainer3");
+// 	$(".jainaContentContainer2").removeClass("jainaContentContainer2");	
+// });
 
+// $(".enemyCharacter2").one("click", function(){
+// 	$(".enemyCharacter2").appendTo(".defenderCharacter1");
+// 	// $(".enemyCharacter2").addClass(".defenderCharacter1");
+// 	$(".enemyCharacter2").removeClass("enemyCharacter2");
+// 	$(".azmodanContentContainer2").addClass("azmodanContentContainer3");
+// 	$(".azmodanContentContainer2").removeClass("azmodanContentContainer2");
+// });
 
+// $(".enemyCharacter3").one("click", function(){
+// 	$(".enemyCharacter3").appendTo(".defenderCharacter1");
+// 	// $(".enemyCharacter3").addClass(".defenderCharacter1");
+// 	$(".enemyCharacter3").removeClass("enemyCharacter3");
+// 	$(".johannaContentContainer2").addClass("johannaContentContainer3");
+// 	$(".johannaContentContainer2").removeClass("johannaContentContainer2");
+// });
 
+// $(".enemyCharacter4").one("click", function(){
+// 	$(".enemyCharacter4").appendTo(".defenderCharacter1");
+// 	// $(".enemyCharacter4").addClass(".defenderCharacter1");
+// 	$(".enemyCharacter4").removeClass("enemyCharacter4");
+// 	$(".murkyContentContainer2").addClass("murkyContentContainer3");
+// 	$(".murkyContentContainer2").removeClass("murkyContentContainer2");
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Create header
-// var starwarsHeader = $("<h1>");
-// starwarsHeader.html("Star Wars RPG!");
-// $("#header").append(starwarsHeader);
-// starwarsHeader.attr("id", "headerAdjustments");
-
-
-// Your Character Text
-// var yourCharText = $("<h3>");
-// yourCharText.html("Your Character");
-// $("#characterText").append(yourCharText);
-// yourCharText.attr("id", "yourCharText");
-
-
-// Enemies Avaiable to attack text
-// var enemiesCharText = $("<h3>");
-// enemiesCharText.html("Enemies Avaiable To Attack");
-// $("#enemiesText").append(enemiesCharText);
-// enemiesCharText.attr("id", "enemiesCharText");
-
-
-// Fight Text
-// var fightCharText = $("<h3>");
-// fightCharText.html("Fight Section");
-// $("#fightText").append(fightCharText);
-// fightCharText.attr("id", "fightCharText");
-
-
-// Yoda Changes
-// var yodaImgContainer = $("<div>");
-// $("#yoda").append(yodaImgContainer);
-// yodaImgContainer.attr("id", "yodaImgContainer");
-
-// var yodaName = $("<p>");
-// yodaName.html(yoda.name);
-// $("#yodaImgContainer").append(yodaName);
-// yodaName.attr("id", "yodaName");
-
-// var yodaImg = $("<img>");
-// $("#yodaImgContainer").append(yodaImg);
-// yodaImg.attr("src", "assets/images/1-yoda.jpg");
-// yodaImg.attr("id", "yodaImg");
-
-// var yodaStats = $("<p>");
-// yodaStats.html("<p>Current HP: " + yoda.healthPoints + "</p>" +
-// 	"<p>Attack Power: " + yoda.attackPower + "</p>" +
-// 	"<p>Counter Attack Power: " + yoda.counterAttackPower + "</p>");
-// $("#yodaImgContainer").append(yodaStats);
-// yodaStats.attr("id", "yodaStats");
-
-
-// Darth Vader Changes
-// var darthVaderImgContainer = $("<div>");
-// $("#darth-vader").append(darthVaderImgContainer);
-// darthVaderImgContainer.attr("id", "darthVaderImgContainer");
-
-// var darthVaderName = $("<p>");
-// darthVaderName.html(darthVader.name)
-// $("#darthVaderImgContainer").append(darthVaderName);
-// darthVaderName.attr("id", "darthVaderName");
-
-// var darthVaderImg = $("<img>");
-// $("#darthVaderImgContainer").append(darthVaderImg);
-// darthVaderImg.attr("src", "assets/images/2-darth-vader.jpg");
-// darthVaderImg.attr("id", "darthVaderImg");
-
-// var darthVaderStats = $("<p>");
-// darthVaderStats.html("<p>Current HP: " + darthVader.healthPoints + "</p>" +
-// 	"<p>Attack Power: " + darthVader.attackPower + "</p>" +
-// 	"<p>Counter Attack Power: " + darthVader.counterAttackPower + "</p>");
-// $("#darthVaderImgContainer").append(darthVaderStats);
-// darthVaderStats.attr("id", "darthVaderStats");
-
-
-// Grievous Changes
-// var grievousImgContainer = $("<div>");
-// $("#grievous").append(grievousImgContainer);
-// grievousImgContainer.attr("id", "grievousImgContainer");
-
-// var grievousName = $("<p>");
-// grievousName.html(grievous.name)
-// $("#grievousImgContainer").append(grievousName);
-// grievousName.attr("id", "grievousName");
-
-// var grievousImg = $("<img>");
-// $("#grievousImgContainer").append(grievousImg);
-// grievousImg.attr("src", "assets/images/3-grievous.jpg");
-// grievousImg.attr("id", "grievousImg");
-
-// var grievousStats = $("<p>");
-// grievousStats.html("<p>Current HP: " + grievous.healthPoints + "</p>" +
-// 	"<p>Attack Power: " + grievous.attackPower + "</p>" +
-// 	"<p>Counter Attack Power: " + grievous.counterAttackPower + "</p>");
-// $("#grievousImgContainer").append(grievousStats);
-// grievousStats.attr("id", "grievousStats");
-
-
-// Count Dooku Changes
-// var countDookuImgContainer = $("<div>");
-// $("#count-dooku").append(countDookuImgContainer);
-// countDookuImgContainer.attr("id", "countDookuImgContainer");
-
-// var countDookuName = $("<p>");
-// countDookuName.html(countDooku.name)
-// $("#countDookuImgContainer").append(countDookuName);
-// countDookuName.attr("id", "countDookuName");
-
-// var countDookuImg = $("<img>");
-// $("#countDookuImgContainer").append(countDookuImg);
-// countDookuImg.attr("src", "assets/images/4-count-dooku.jpg");
-// countDookuImg.attr("id", "countDookuImg");
-
-// var countDookuStats = $("<p>");
-// countDookuStats.html("<p>Current HP: " + countDooku.healthPoints + "</p>" +
-// 	"<p>Attack Power: " + countDooku.attackPower + "</p>" +
-// 	"<p>Counter Attack Power: " + countDooku.counterAttackPower + "</p>");
-// $("#countDookuImgContainer").append(countDookuStats);
-// countDookuStats.attr("id", "countDookuStats");
-
-// console.log(darkVader.counterAttackPower);
