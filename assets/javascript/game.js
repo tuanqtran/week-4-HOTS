5ece58f20bc9e813a8a7b78a86b41f45adb5b919
Tuan Tran <tuan.tran1122@gmail.com>
2016-08-20 15:08:38 -0500

    Added sounds and changes to JS. Work to be Continued.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+var themeSong = new Audio("assets/sounds/themeSong.mp3");
+
 var jaina = {
 	"name": "Jaina",
 	"healthPoints": 100,
@@ -59,199 +61,97 @@ var murkyStatsUpdate = $(".murky");
 	murkyStatsUpdate.attr("counterAttackPower", murky.counterAttackPower);
 
 function resetGame(){
-
+	location.reload(true);
 }
 
-$(".chosenCharacters").click(function(){
-	$(this).removeClass("nonChosenCharacters");
-	$(".nonChosenCharacters").removeClass("chosenCharacters")
-		.removeClass("nonChosenCharacters")
-		.appendTo(".enemiesCharacters")
-		.addClass("enemyCharacter");
-	$(".chosenCharacters .counterAttackPower").detach();
+$(document).ready(function(){
+
+	themeSong.play();
+
+	$(".chosenCharacters").click(function(){
+		$(this).removeClass("nonChosenCharacters");
+		$(".nonChosenCharacters").removeClass("chosenCharacters")
+			.removeClass("nonChosenCharacters")
+			.appendTo(".enemiesCharacters")
+			.addClass("enemyCharacter");
+		$(".chosenCharacters .counterAttackPower").detach();
+
+		$(".defeated").html("You have chosen " + $('.chosenCharacters').attr('name') + " as your character.");
+
+		$(".enemyCharacter .contentContainer").removeClass("contentContainer")
+			.addClass("contentContainer2");
+		$(".enemyCharacter .attackPower").detach();
 
 
-	$(".enemyCharacter .contentContainer").removeClass("contentContainer")
-		.addClass("contentContainer2");
-	$(".enemyCharacter .attackPower").detach();
+	    if ($(".defenderSection").is(":empty") && $(this).hasClass("enemyCharacter")) {
+	    	$(this).appendTo(".defenderSection")
+	    		.removeClass("enemyCharacter")
+	    		.addClass("defenderCharacter");
+	    		
+			$(".defenderCharacter .contentContainer2").removeClass("contentContainer2")
+				.addClass("contentContainer3");
+
+			$(".defeated").html("You have chosen " + $('.defenderCharacter').attr('name') + " to fight.");
+			$(".attackDmgInfo").html("");
+			$(".defenderDmgInfo").html("");
+
+	    }
+	});
 
 
-    if ($(".defenderSection").is(":empty") && $(this).hasClass("enemyCharacter")) {
-    	$(this).appendTo(".defenderSection")
-    		.removeClass("enemyCharacter")
-    		.addClass("defenderCharacter");
-    		
-		$(".defenderCharacter .contentContainer2").removeClass("contentContainer2")
-			.addClass("contentContainer3");
-    }
+	$(".attackButton").on("click", function(){
+		if ($(".defenderSection").is(":empty")){
+
+		}else{
+
+			$(".defeated").html("");
+
+			defenderCurrentHealth = parseInt($(".defenderCharacter").attr("healthPoints")) - parseInt($(".chosenCharacters").attr("attackPower"));
+			$(".defenderCharacter").attr("healthPoints", defenderCurrentHealth);
+			$(".defenderCharacter .healthPoints").html("Current HP: " + defenderCurrentHealth);	
+
+
+			var attackPowerIncrease = parseInt($(".chosenCharacters").attr('attackPower')) + parseInt($(".chosenCharacters").attr('basePower'));
+			$(".chosenCharacters").attr("attackPower", attackPowerIncrease);
+			$(".chosenCharacters .attackPower").html("Attack Power: " + attackPowerIncrease);		
+
+
+			$(".attackDmgInfo").html($('.chosenCharacters').attr('name') + " has done " + attackPowerIncrease + " dmg to " + $('.defenderCharacter').attr('name'));
+			$(".defenderDmgInfo").html($('.defenderCharacter').attr('name') + " has done " + $('.defenderCharacter').attr('counterAttackPower') + " dmg to " + $('.chosenCharacters').attr('name'));
+
+			if (defenderCurrentHealth <= 0){
+				$(".defeated").html("You have defeated " + $(".defenderCharacter").attr("name"));
+				$(".defenderSection").empty();
+			}
+
+			if (defenderCurrentHealth > 0){
+				var chosenCharacterCurrentHealth = parseInt($(".chosenCharacters").attr("healthPoints")) - parseInt($(".defenderCharacter").attr("counterAttackPower"));
+				$(".chosenCharacters").attr("healthPoints", chosenCharacterCurrentHealth);
+				$(".chosenCharacters .healthPoints").html("Current HP: " + chosenCharacterCurrentHealth);
+			}
+
+			if ($(".chosenCharacters").attr("healthPoints") <= 0){
+				$(".defenderText h3").html($(".defenderCharacter").attr("name") + " win!")
+				
+				$(".fightInfo").html("<p class='defeated'>You have lost to try again.</p>" +
+					"<button class='resetButton'>Reset Game</button");
+				$(".fightInfo button").on("click", function(){
+					resetGame();
+				})
+				$(".chosenCharacters").empty();		
+			}
+			
+			if ($(".enemiesCharacters").is(":empty") && $(".defenderSection").is(":empty") &&
+				$(".chosenCharacters").attr("healthPoints") > 0 && $(".johanna").attr("healthPoints") != 400){
+				$(".chosenCharacters").appendTo(".defenderSection");
+				$(".defenderText h3").html($(".chosenCharacters").attr("name") + " win!")
+
+
+				$(".fightInfo").html("<button class='resetButton'>Reset Game</button")
+				$(".fightInfo button").on("click", function(){
+					resetGame();
+				});
+			}
+		}
+	})
 });
-
-
-$(".attackButton").on("click", function(){
-
-	defenderCurrentHealth = parseInt($(".defenderCharacter").attr("healthPoints")) - parseInt($(".chosenCharacters").attr("attackPower"));
-	$(".defenderCharacter").attr("healthPoints", defenderCurrentHealth);
-	$(".defenderCharacter .healthPoints").html("Current HP: " + defenderCurrentHealth);	
-
-	// Create a if statement that will avoid state increase at start of game.
-	// if (){	
-		var attackPowerIncrease = parseInt($(".chosenCharacters").attr('attackPower')) + parseInt($(".chosenCharacters").attr('basePower'));
-		$(".chosenCharacters").attr("attackPower", attackPowerIncrease);
-		$(".chosenCharacters .attackPower").html("Attack Power: " + attackPowerIncrease);		
-	// }
-
-	if (defenderCurrentHealth <= 0){
-		$(".fightInfo").html("<p class='defeated'>You have defeated " + $(".defenderCharacter").attr("name") +"</p>");	
-		$(".defenderSection").empty();
-	}
-
-	if (defenderCurrentHealth > 0){
-		var chosenCharacterCurrentHealth = parseInt($(".chosenCharacters").attr("healthPoints")) - parseInt($(".defenderCharacter").attr("counterAttackPower"));
-		$(".chosenCharacters").attr("healthPoints", chosenCharacterCurrentHealth);
-		$(".chosenCharacters .healthPoints").html("Current HP: " + chosenCharacterCurrentHealth);
-	}
-
-	if ($(".chosenCharacters").attr("healthPoints") <= 0){
-		$(".defenderText h3").html($(".defenderCharacter").attr("name") + " win!")
-		
-		$(".fightInfo").html("<p class='defeated'>You have lost to try again.</p>" +
-			"<button class='resetButton'>Reset Game</button");
-		$(".fightInfo button").on("click", function(){
-			reset();
-		})
-		$(".chosenCharacters").empty();		
-	}
-	
-	if ($(".enemiesCharacters").is(":empty") && $(".defenderSection").is(":empty") &&
-		$(".chosenCharacters").attr("healthPoints") > 0 && $(".johanna").attr("healthPoints") != 400){
-		$(".chosenCharacters").appendTo(".defenderSection");
-		$(".defenderText h3").html($(".chosenCharacters").attr("name") + " win!")
-
-
-		$(".fightInfo").html("<button class='resetButton'>Reset Game</button")
-		$(".fightInfo button").on("click", function(){
-			reset();
-		});
-	}
-
-})
-
-
-// var attackIncrease = parseInt($(".champ").attr('attackPower')) + parseInt($(".champ").attr('baseAttack'))
-// $(".champ").attr("attackPower", attackIncrease);
-
-// $(".btn").click(function(){
- // var defenderHealthLoss = parseInt($(".defender").attr('healthPoints')) - parseInt($(".champ").attr('attackPower'));
-// $(".defender").attr("healthPoints", defenderHealthLoss);
-// var updateNumber = (defenderHealthLoss);
-// document.querySelector(".grumpyP").innerHTML = updateNumber;
-//  console.log("Grumpy cat's health: " + defenderHealthLoss);
- 		//var counterAttack = parseInt($(".champ").attr('healthPoints')) - parseInt($(".defender").attr('counterAttackPower'));
- 		//defender counter attacking champ
-// })
-
-
-
-
-
-
-// $(".enemiesCharacters").click(function(){
-// 	// alert("Enemy Character has been clicked");
-// 	console.log(this);
-// 	if($(".defenderSection").is(":empty")){
-// 		alert("Empty");
-// 	}else{
-// 		alert("Not working");
-// 	}
-// });
-
-
-// $(".yourCharacter1").one("click", function(){
-// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
-// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
-
-// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
-// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
-
-// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
-// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-// });
-
-// $(".yourCharacter2").one("click", function(){
-// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
-// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
-// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
-
-// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
-// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-// });
-
-// $(".yourCharacter3").one("click", function(){
-// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
-// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
-// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
-
-// 	$(".yourCharacter4").appendTo(".enemiesCharacters").addClass("enemyCharacter4").removeClass("yourCharacter4");
-// 	$(".murkyContentContainer").addClass("murkyContentContainer2").removeClass("murkyContentContainer");
-
-// });
-
-// $(".yourCharacter4").one("click", function(){
-// 	$(".yourCharacter1").appendTo(".enemiesCharacters").addClass("enemyCharacter1").removeClass("yourCharacter1");
-// 	$(".jainaContentContainer").addClass("jainaContentContainer2").removeClass("jainaContentContainer");
-
-// 	$(".yourCharacter2").appendTo(".enemiesCharacters").addClass("enemyCharacter2").removeClass("yourCharacter2");
-// 	$(".azmodanContentContainer").addClass("azmodanContentContainer2").removeClass("azmodanContentContainer");
-
-// 	$(".yourCharacter3").appendTo(".enemiesCharacters").addClass("enemyCharacter3").removeClass("yourCharacter3");
-// 	$(".johannaContentContainer").addClass("johannaContentContainer2").removeClass("johannaContentContainer");
-
-// });
-
-
-
-
-
-
-
-
-
-
-// $(".enemyCharacter1").one("click", function(){
-// 	$(".enemyCharacter1").appendTo(".defenderCharacter1");
-// 	// $(".enemyCharacter1").addClass(".defenderCharacter1");
-// 	$(".enemyCharacter1").removeClass("enemyCharacter1");
-// 	$(".jainaContentContainer2").addClass("jainaContentContainer3");
-// 	$(".jainaContentContainer2").removeClass("jainaContentContainer2");	
-// });
-
-// $(".enemyCharacter2").one("click", function(){
-// 	$(".enemyCharacter2").appendTo(".defenderCharacter1");
-// 	// $(".enemyCharacter2").addClass(".defenderCharacter1");
-// 	$(".enemyCharacter2").removeClass("enemyCharacter2");
-// 	$(".azmodanContentContainer2").addClass("azmodanContentContainer3");
-// 	$(".azmodanContentContainer2").removeClass("azmodanContentContainer2");
-// });
-
-// $(".enemyCharacter3").one("click", function(){
-// 	$(".enemyCharacter3").appendTo(".defenderCharacter1");
-// 	// $(".enemyCharacter3").addClass(".defenderCharacter1");
-// 	$(".enemyCharacter3").removeClass("enemyCharacter3");
-// 	$(".johannaContentContainer2").addClass("johannaContentContainer3");
-// 	$(".johannaContentContainer2").removeClass("johannaContentContainer2");
-// });
-
-// $(".enemyCharacter4").one("click", function(){
-// 	$(".enemyCharacter4").appendTo(".defenderCharacter1");
-// 	// $(".enemyCharacter4").addClass(".defenderCharacter1");
-// 	$(".enemyCharacter4").removeClass("enemyCharacter4");
-// 	$(".murkyContentContainer2").addClass("murkyContentContainer3");
-// 	$(".murkyContentContainer2").removeClass("murkyContentContainer2");
-// });
-
